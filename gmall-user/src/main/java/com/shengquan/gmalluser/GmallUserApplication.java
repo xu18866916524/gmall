f4cbb7bcac11260c5d202bb4c7533bf923ba8254
SKY-20180124PWR\Administrator <xu18866916524@163.com>
2020-05-07 09:26:09 +0800

    创建项目提交

--- gmall-user/src/main/java/com/shengquan/gmalluser/GmallUserApplication.java
+++ gmall-user/src/main/java/com/shengquan/gmalluser/GmallUserApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GmallUserApplication {
 
 	public static void main(String[] args) {
-		System.out.println("用户服务");
+		System.out.println("用户服务启动");
 		SpringApplication.run(GmallUserApplication.class, args);
 	}
 
